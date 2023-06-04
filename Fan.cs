5e99c3d9593cb566bb0d93d5f1e2438f1425dedb
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-06-05 01:27:32 +0700

    lan1

---
+++ Fan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fan
+{
+    public class Fan
+    {
+        private const int slow = 1;
+        private const int medium = 2;
+        private const int fast = 3;
+        private int speed;
+        private bool on;
+        private double radius;       
+        private string color;
+
+        public static int Slow => slow;
+
+        public static int Medium => medium;
+
+        public static int Fast => fast;
+
+        public int Speed { get => speed; set => speed = value; }
+        public bool On { get => on; set => on = value; }
+        public double Radius { get => radius; set => radius = value; }
+        public string Color { get => color; set => color = value; }
+        public Fan()
+        {
+            speed = slow;
+            on = false;
+            radius = 5;
+            color = "blue";
+        }
+        public override string ToString()
+        {
+            if (on)
+            {
+                return $"Speed: {speed}, Color: {color}, Radius: {radius} - Fan is on";
+            }
+            else
+            {
+                return $"Color: {color}, Radius: {radius} - Fan is off";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan fan1 = new Fan();
+            fan1.Speed = 3;
+            fan1.Radius = 10;
+            fan1.Color = "yellow";
+            fan1.On = true;
+
+            Fan fan2 = new Fan();
+            fan2.Speed = 2;
+            fan2.Radius = 5;
+            fan2.Color = "blue";
+            fan2.On = false;
+
+            Console.WriteLine("Fan 1: " + fan1.ToString());
+            Console.WriteLine("Fan 2: " + fan2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
